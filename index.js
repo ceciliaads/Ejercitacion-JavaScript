ffcc6f52c7df4d4ab709a6500c9d35c407bb36d3
Cecilia Da Silva <ceciliadasilva@Cecilias-MacBook-Air.local>
2020-08-11 20:09:57 -0300

    heladeria - hecho

---
+++ index.js
@@ -0,0 +1,10 @@
+let primerGusto = prompt("Decime tu gusto de helado favorito")
+
+let segundoGusto = prompt("Decime tu segundo gusto favorito")
+
+let tercerGusto = prompt("Decime tu tercer gusto favorito")
+
+let gustosCompletos = primerGusto + " " + segundoGusto + " " + tercerGusto
+
+
+alert("Aquí tiene su helado de " + gustosCompletos)
